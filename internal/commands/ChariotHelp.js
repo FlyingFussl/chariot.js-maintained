a21b28eaeed05e59d43b18513987dacac4797948
FlyingFussl <96882928+FlyingFussl@users.noreply.github.com>
2022-06-07 11:05:32 +0200

    Interaction Support

--- internal/commands/ChariotHelp.js
+++ internal/commands/ChariotHelp.js
@@ -3,51 +3,55 @@ const Logger = require('../../helpers/Logger');
 
 class ChariotHelp {
     constructor() {
+        this.type = 1
         this.name = 'help';
         this.permissions = ['embedLinks'];
         this.allowDMs = true;
+        this.description = "A menu to get help on the bot's commands";
+        this.defaultPermission = true;
+        this.ephemeral = true;
         this.help = {
             message: 'Get either a general Help card or instructions for specified commands! Specifying a command is optional. If a command was specified its help text will show up.',
             usage: 'help [command]',
             example: ['help', 'help command'],
             inline: true
         }
+        this.options = [{
+            "name": "command",
+            "description": "A specific command to get help for",
+            "type": 3,
+            "required": false
+        }]
     }
 
-    async execute(message, args, chariot) {
-        const prefixHandler = (message.prefix === `<@!${chariot.user.id}> ` || message.prefix === `<@${chariot.user.id}> `) ? `@${chariot.user.username}#${chariot.user.discriminator} ` : message.prefix;
+    async slash(interaction, data, chariot) {
 
-        if (!args[0]) {
+        if (!data.options || !data.options.length) {
             const commandNames = chariot.commands.filter((cmnds) => !cmnds.owner).filter((cmnds) => (cmnds.hasOwnProperty('help')) ? ((cmnds.help.visible === undefined) ? true : !(cmnds.help.visible === false)) : true).map((cmnds) => '`' + cmnds.name + '`');
 
-            return message.channel.createEmbed(new Embed()
+            let em = new Embed()
                 .setColor(chariot.chariotOptions.chariotConfig.primaryColor || 'RANDOM')
                 .setTitle('Command Help')
-                .setDescription(`Get detailed command instructions for any command!\n You can specify a certain command by writing \`${prefixHandler}help <commandName>\`!`)
-                .addField('Commands', commandNames.join(', '))
-            );
+                .setDescription(`Get detailed command instructions for any command!\n You can also specify a command!`)
+                .addField('Commands', commandNames.join(', '));
+
+            return interaction.createMessage({ embed: em });
+
         } else {
-            const foundCommand = chariot.commands.get(args[0]) || chariot.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(args[0]));
+            const foundCommand = chariot.commands.get(data.options[0].value) || chariot.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(data.options[0].value));
 
             if (!foundCommand) {
-                return message.channel.createEmbed(new Embed()
+                let em = new Embed()
                     .setColor('RED')
-                    .setTitle(`Couldn't find command **${args[0]}**!`)
-                );
-            }
-
-            if (foundCommand.owner && !chariot.chariotOptions.chariotConfig.owner.includes(message.author.id)) {
-                return message.channel.createEmbed(new Embed()
-                    .setColor('RED')
-                    .setTitle('Insufficient Permissions!')
-                );
+                    .setTitle(`Couldn't find command **${data.options[0].value}**!`);
+                return interaction.createMessage({ embed: em });
             }
 
             if (foundCommand && !foundCommand.help) {
-                return message.channel.createEmbed(new Embed()
+                let em = new Embed()
                     .setColor('RED')
-                    .setDescription(`Unfortunately command **${foundCommand.name}** has no integrated help text yet.`)
-                );
+                    .setDescription(`Unfortunately command **${foundCommand.name}** has no integrated help text yet.`);
+                return interaction.createMessage(em);
             }
 
             const helpEmbed = new Embed();
@@ -55,16 +59,16 @@ class ChariotHelp {
             helpEmbed.setColor(chariot.chariotOptions.chariotConfig.primaryColor || 'RANDOM');
             helpEmbed.setTitle(`**${foundCommand.name}** Help`);
             helpEmbed.setDescription(foundCommand.help.message || 'No help description available');
-            helpEmbed.addField('Usage', (foundCommand.help.usage) ? `\`${prefixHandler}${foundCommand.help.usage}\`` : 'No usage available', foundCommand.help.inline);
+            helpEmbed.addField('Usage', (foundCommand.help.usage) ? `\`/${foundCommand.help.usage}\`` : 'No usage available', foundCommand.help.inline);
 
             let helpArray = [];
             let exampleText = '';
 
             if (Array.isArray(foundCommand.help.example)) {
-                helpArray = foundCommand.help.example.map((helpItem) => `\`${prefixHandler}${helpItem}\``);
+                helpArray = foundCommand.help.example.map((helpItem) => `\`/${helpItem}\``);
                 exampleText = helpArray.join(', ');
             } else {
-                exampleText = `\`${prefixHandler}${foundCommand.help.example}\``;
+                exampleText = `\`/${foundCommand.help.example}\``;
             }
 
             helpEmbed.addField(Array.isArray(foundCommand.help.example) ? 'Examples' : 'Example', exampleText, foundCommand.help.inline);
@@ -74,8 +78,7 @@ class ChariotHelp {
                 helpEmbed.addField('Aliases', commandAliases.join(', '), false);
             }
 
-            message.channel.createEmbed(helpEmbed);
-            Logger.success('HELP', `${message.author.username}#${message.author.discriminator} (${message.author.id}) used the help command for command ${foundCommand.name}.`);
+            return interaction.createMessage({ embed: helpEmbed });
         }
     }
 }
